2e0d93fcd4ceac4b02038a1c02dea379800cae3e
sergioRodriguez <sandres0190@gmail.com>
2023-08-05 19:28:50 -0500

    HU Se configura retrofit con Hilt, en esta parte tambien se configuro Firebase con HIlt

---
+++ app/src/main/java/com/alternova/lego/data/local/database/dao/UserDao.kt
@@ -0,0 +1,23 @@
+package com.alternova.lego.data.local.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.alternova.lego.data.local.database.entity.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(user: UserEntity)
+
+    @Query(
+        """
+        SELECT * FROM ${UserEntity.TABLE_NAME} WHERE id = :idUser
+        """
+    )
+    fun getUserById(idUser: Int): Flow<UserEntity>
+
+}
